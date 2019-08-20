b164642ce5f6ea66feb64d63dc6a2b8c057dfb99
pranshu469 <pranshu469@gmail.com>
2019-08-20 20:31:31 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,50 @@
+package log4jdemo;
+import java.io.IOException;
+import java.sql.*;
+import java.util.InputMismatchException;
+import org.apache.log4j.Logger;
+
+public class Div 
+{
+    private static final Logger logger=Logger.getLogger(Log4jDemo.class.getName());
+    public void div() throws IOException
+    {
+         int x1=0,y1=0,exc=0;
+        System.out.println("enter 2 operands");
+        try{
+        x1=Log4jDemo.sc.nextInt();
+        y1=Log4jDemo.sc.nextInt();
+        }catch(InputMismatchException e){
+            exc=100;
+            System.out.println("operands must be integer");
+        }
+        if(exc!=100)
+        {
+         if(y1!=0){
+           double z1=x1*1.0/y1;
+                System.out.println("result of division is ");
+                System.out.format("%.3f", z1);
+                try{
+                    String t1="div";
+                    Statement s=Log4jDemo.c.createStatement();
+                    int x=s.executeUpdate("insert into "+Log4jDemo.p.getProperty("tb")+" values('"+t1+"','"+x1+"','"+y1+"','"+z1+"')");
+                    ResultSet rs=s.executeQuery("select * from "+Log4jDemo.p.getProperty("tb"));
+                      }catch(SQLException e){
+                                logger.error("error occured "+e);
+                                 }
+            }
+        else{
+                logger.error("denominator can not be zero");
+            Log4jDemo l4;
+            l4 = new Log4jDemo();
+            }
+    }
+}
+}
+
+
+
+
+
+ 
+ 
---
+++ Mul.java
@@ -0,0 +1,47 @@
+package log4jdemo;
+
+
+import java.io.IOException;
+
+import java.sql.*;
+import java.util.InputMismatchException;
+
+import org.apache.log4j.Logger;
+
+public class Mul
+{
+   private static final Logger logger=Logger.getLogger(Log4jDemo.class.getName());
+    public void mul() throws IOException
+    {
+         int x1=0,y1=0,z1=0,exc=0;
+       System.out.println("enter 2 operands");
+        
+       try{
+           x1=Log4jDemo.sc.nextInt();
+           y1=Log4jDemo.sc.nextInt();
+        }catch(InputMismatchException e){
+            exc=100;
+            System.out.println("operands must be integer");
+        }
+        
+        if(exc!=100)
+        {
+        z1=x1*y1;
+        
+        System.out.println("result of multiplication is "+z1);
+        try{
+            String t1="mul";
+            Statement s=Log4jDemo.c.createStatement();
+            int x=s.executeUpdate("insert into "+Log4jDemo.p.getProperty("tb")+" values('"+t1+"','"+x1+"','"+y1+"','"+z1+"')");
+            ResultSet rs=s.executeQuery("select * from "+Log4jDemo.p.getProperty("tb"));
+            }catch(Exception e){
+            logger.error("error occured "+e);
+        }
+        }
+        else{
+            
+            System.out.println("error in operands");
+            
+        }
+    }
+}
